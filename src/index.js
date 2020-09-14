7ac76c9784af730164557c58c8284bbe25f5bfc0
Graham Dodsworth <graham.dodsworth@nhs.net>
2020-09-14 22:27:55 +0100

    add login functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const oktaConfig = {
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+  redirect_uri: `${window.location.origin}/implicit/callback`,
+  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Security {...oktaConfig}>
+      <App />
+    </Security>
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
